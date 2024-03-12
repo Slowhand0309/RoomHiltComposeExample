8d059c9bc0c1a6b7a5e16623f3e72da8065cbb76
Slowhand0309 <slowhand0309@gmail.com>
2024-03-12 22:23:01 +0900

    add AppDatabase class

---
+++ app/src/main/java/com/example/roomhiltcomposeexample/data/AppDatabase.kt
@@ -0,0 +1,13 @@
+package com.example.roomhiltcomposeexample.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [User::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+}
